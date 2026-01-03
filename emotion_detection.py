d93c145b157bb526bac3104db76ad7284bf78246
Sharmila <jinka.s.learning@gmail.com>
2026-01-03 05:00:24 -0500

    Task3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 ''' This function uses Watson NLP Library for Emotion Detection
 '''
@@ -7,4 +8,19 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     request = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=request, headers=headers)
-    return response.text
+    
+    json_formatted_response = json.loads(response.text)
+    emotion_dict = json_formatted_response["emotionPredictions"][0]["emotion"]
+    dominant_emotion, highest_score = max(emotion_dict.items(), key=lambda item: item[1])
+    formatted_response = {
+                        'anger': json_formatted_response['emotionPredictions'][0]['emotion']['anger'],
+                        'disgust': json_formatted_response['emotionPredictions'][0]['emotion']['disgust'],
+                        'fear': json_formatted_response['emotionPredictions'][0]['emotion']['fear'],
+                        'joy': json_formatted_response['emotionPredictions'][0]['emotion']['joy'],
+                        'sadness': json_formatted_response['emotionPredictions'][0]['emotion']['sadness'],
+                        'dominant_emotion': dominant_emotion
+                    }
+
+
+
+    return formatted_response
